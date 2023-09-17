6aae4a1202ad38e11efd228061ee80d032c9d8c3
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-17 14:19:12 -0700

    TSK1

---
+++ int_print.c
@@ -0,0 +1,57 @@
+#include <main.h>
+
+/**
+ * pr - print single char
+ * @c: parameter
+ * Return: Always 0.
+ */
+
+int pr(char c)
+{
+return (write(1, &c, 1));
+}
+/**
+ * deci - print int
+ * @b: parameter
+ * Return: Always 0.
+ */
+void deci(int b)
+{
+if (b < 0)
+{
+b = b * -1;
+pr('-');
+}
+if (b == 0)
+return;
+deci(b / 10);
+pr(((b % 10) + '0'));
+}
+
+/**
+ * _printf - print args of int
+ * @s: parameter
+ * Return: Always 0.
+ */
+void _printf(char *s, ...)
+{
+int i, j = 0, count = 0, m;
+while (s[j] != '\0')
+j++;
+for (i = 0; i < j; i++)
+{
+if ((s[i] == '%') && ((s[i + 1] == 'd' || s[i + 1] == 'i')))
+count++;
+}
+va_list args;
+va_start(args, count);
+for (i = 0; i < j; i++)
+{
+if ((s[i] == '%') && ((s[i + 1] == 'd' || s[i + 1] == 'i')))
+{
+m = va_arg(args, int);
+deci(m);
+}}
+va_end(args);
+}
+
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdarg.h>
+void _printf(char *s, ...);
+int pr(char c);
+void deci(int b);
+
+
+
+#endif
