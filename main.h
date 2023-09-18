5a4c40697c751010e2c1c741e34f796332fa4da3
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-18 09:44:03 -0700

    T

--- main.h
+++ main.h
@@ -1,11 +1,11 @@
 #ifndef main_h
 #define main_h
 #include <stdio.h>
-#include <unistd.h>
+#include <unist.h>
 #include <stdarg.h>
+void deci(int b);
 int _printf(const char *format, ...);
 int pr(char c);
-void deci(int b);
 void str(char *s);
 
 
