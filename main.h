340fd725b89f786770f692c14f45904558a4009b
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-19 07:09:30 -0700

    d

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * struct mix - Short description
+ * @mix: type char pointer
+ * @func: pointer to function.
+ *
+ * Description: Longer description
+ */
+typedef struct mix
+{
+	char *mix;
+	int (*func)();
+} mix_t;
+void test_deci(va_list bb);
+void test_str(va_list ss);
+void deci(int b);
+int _printf(const char *format, ...);
+int pr(char c);
+void str(char *s);
+int str_printf(const char *format, ...);
+int int_printf(const char *format, ...);
+#endif
