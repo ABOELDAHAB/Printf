6f69c7d9588930bcd6efb7896ab0ac789754837b
Nada32 <52667595+Nada32@users.noreply.github.com>
2023-09-19 16:25:05 +0300

    T

--- test_print.c
+++ test_print.c
@@ -7,18 +7,18 @@
  */
 int _printf(const char * const format, ...)
 {
-mix_t xm[]={{"%s",str_print},{"%i",int_print},{"%d",int_print}}
+mix_t xm[] = {{"%s", str_print}, {"%i", int_print}, {"%d", int_print}}
 int i, j, x;
 va_list args;
 va_start(args, format)
 
-for(i = 0; format != '\0'; i++)
+for (i = 0; format != '\0'; i++)
 {
-for(j = 0; j < 3; j++)
+for (j = 0; j < 3; j++)
 {
-if ((format[i] == xm[j].mix[0]) && (format[i+1] == xm[j].mix[1]))
+if ((format[i] == xm[j].mix[0]) && (format[i + 1] == xm[j].mix[1]))
 xm[j].func;
-else if (!((format[i-1] == xm[j].mix[0]) && (format[i] == xm[j].mix[1])))
+else if (!((format[i - 1] == xm[j].mix[0]) && (format[i] == xm[j].mix[1])))
 pr(format[i]);
 }}
 va_end(args);
