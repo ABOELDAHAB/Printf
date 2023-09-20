e780359d913731d6c6ff77fb33bd2240ae65ab6d
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-19 19:39:16 -0700

    T

--- test_print.c
+++ test_print.c
@@ -20,9 +20,11 @@ for (i = 0; format[i] != '\0'; i++)
 for (j = 0; j < 4; j++)
 {
 if ((format[i] == xm[j].mix[0]) && (format[i + 1] == xm[j].mix[1]))
+{
+if (((format[i - 1] != xm[j].mix[0]) && (format[i] != xm[j].mix[1])))
 {xm[j].func(args);
 i = i + 2;
-}}
+}}}
 if ((format[i] != '\0') && !((format[i] == '%')
 && (format[i + 1] == '%')))
 pr(format[i]);
