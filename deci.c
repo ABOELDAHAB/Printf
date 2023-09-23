b146b9b60e7afa616e2d46649a567ece17237989
Nada32 <52667595+Nada32@users.noreply.github.com>
2023-09-23 19:28:25 +0300

    T

--- deci.c
+++ deci.c
@@ -7,15 +7,18 @@
  */
 int deci(int b)
 {
-int i = 0, ii = 0;
+int i = 0, ii = 0, iii = 0;
 if (b < 0)
 {
 b = b * -1;
 pr('-');
 ii++;
 }
-if (b == 0)
-return (0);
+if ((b / 10) == 0)
+{
+iii = 1 + pr(b + '0');
+return (iii + i - 1);
+}
 i = 1 + deci(b / 10) + ii;
 pr(((b % 10) + '0'));
 return (i);
