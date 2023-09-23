9e7f5b34e02a0175473bd1611bf79500e95d9f83
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-23 08:29:57 -0700

    T

--- deci.c
+++ deci.c
@@ -7,15 +7,16 @@
  */
 int deci(int b)
 {
-int i = 0;
+int i = 0, ii = 0;
 if (b < 0)
 {
 b = b * -1;
 pr('-');
+ii++;
 }
 if (b == 0)
 return (0);
-i = 1 + deci(b / 10);
+i = 1 + deci(b / 10) + ii;
 pr(((b % 10) + '0'));
 return (i);
 }
