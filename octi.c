2f734e9ac21207750ca008da6e342025e3005681
Nada32 <52667595+Nada32@users.noreply.github.com>
2023-09-24 02:27:47 +0300

    T

--- octi.c
+++ octi.c
@@ -7,19 +7,17 @@
  */
 int octi(long int b)
 {
-int i = 0, ii = 0, iii = 0;
+int i = 0, iii = 0;
 if (b < 0)
 {
-b = b * -1;
-pr('-');
-ii++;
+b = 4294967296 + b;
 }
 if ((b / 8) == 0)
 {
 iii = 1 + pr(b + '0');
 return (iii + i - 1);
 }
-i = 1 + octi(b / 8) + ii;
+i = 1 + octi(b / 8);
 pr(((b % 8) + '0'));
 return (i);
 }
