e7485af4cd5091022512722aea0bf92bc64addcd
Nada32 <nada0abou0alsoud@gmail.com>
2023-09-22 17:10:09 -0700

    T

--- test_print.c
+++ test_print.c
@@ -9,35 +9,30 @@ int _printf(const char * const format, ...)
 {
 mix_t xm[] = {{"%s", test_str}, {"%i", test_deci},
 {"%c", test_char}, {"%d", test_deci}};
-int i = 0, ii = 0, j;
-char dod;
+int i = 0, len = 0, ii = 0, j, x = 0;
+
 va_list args;
 va_start(args, format);
 
-if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-return (-1);
 for (i = 0; format[i] != '\0'; i++)
 {
-for (j = 0; j < 4; j++)
+for (j = 0; j < 4 ; j++)
 {
-if (((format[i] == xm[j].mix[0]) && (format[i + 1] == xm[j].mix[1]))
-&& ((format[i] == xm[j].mix[0]) && (format[i + 1] == xm[j].mix[1])))
+if (xm[j].mix[0] == format[i] && xm[j].mix[1] == format[i + 1])
 {
 xm[j].func(args);
-ii = i + 1;
-}
-else if (((format[i - 1] != xm[j].mix[0]) && (format[i] != xm[j].mix[1]))
-&& ((format[i] != xm[j].mix[0]) && (format[i + 1] != xm[j].mix[1])))
-{
-dod = format[i];
-ii = i + 1;
+i = i + 2;
+len = len + 2;
 }
 else
-dod = '\0';
+ii = i;
 }
-if (dod != '\0')
-pr(dod);
+pr(format[i]);
+ii++;
 }
+if (len >= 2)
+x = len - 1;
+i = ii - x;
 va_end(args);
-return (ii);
+return (i);
 }
